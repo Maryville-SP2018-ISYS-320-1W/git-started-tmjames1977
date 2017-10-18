7e34ec1ea42f425195ab3108e716c363c21294ec
Joe Dent <jdent@maryville.edu>
2017-10-18 12:36:35 -0500

    initial commit

---
+++ src/AboutYou.java
@@ -0,0 +1,13 @@
+
+public class AboutYou {
+
+	public static void main(String[] args) {
+		// TODO Modify the values in the String literals in ALL_CAPS below
+		System.out.print("Hello, my name is ");
+		System.out.println("YOUR_NAME");
+
+		System.out.print("And I'm taking this class because ");
+		System.out.println("BECAUSE_WHY");
+	}
+
+}
