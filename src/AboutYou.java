41b0781a5cabb4d53a9a2512382dec565eb963bf
tmjames1977 <33112355+tmjames1977@users.noreply.github.com>
2018-03-20 17:25:46 -0500

    GitHub Assignment Complete
    Completed GitHub Assignment for Submission.

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Timothy James");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("I want to learn how to be a better programmer when using JavaScript.");
 	}
 
 }
